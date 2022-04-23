bcbf5b82deff51f322f10e548d8689f76b937852
kissmark rivas <rivaskissmark@gmail.com>
2022-04-23 15:23:28 -0500

    arreglos

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-function aliensWars(dias = 0,aliens = []) {
+function aliensWars(diasTotales = 0,aliens = []) {
   //ordenar aliens
-  let diasAlien = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  let diasVidaAlien = [0, 0, 0, 0, 0, 0, 0, 0, 0]
   // 0, 1, 2, 3, 4, 5, 6, 7, 8
   for (let i = 0; i < aliens.length; i++) {
-    diasAlien[aliens[i]] += 1
+    diasVidaAlien[aliens[i]] += 1
   }
   
-  for (let pasDia = 0; pasDia < dias; pasDia++) {
-    let primerDia = diasAlien[0]
-    for (let index = 0; index < diasAlien.length; index++) {       
+  for (let diaSiguiente = 0; diaSiguiente < diasTotales; diaSiguiente++) {
+    let primerDia = diasVidaAlien[0] //[0]
+    for (let index = 0; index < diasVidaAlien.length; index++) {       
       if (index !== 8) {
-        diasAlien[index] = diasAlien[index + 1]
+        diasVidaAlien[index] = diasVidaAlien[index + 1]//[1,1]
       } else {
-        diasAlien[6] = diasAlien[6] + primerDia
-        diasAlien[8] = primerDia
+        diasVidaAlien[6] = diasVidaAlien[6] + primerDia
+        diasVidaAlien[8] = primerDia
       }
     }
   }
-  return [diasAlien,diasAlien.reduce((a, b) => a + b, 0)]
+  return [diasVidaAlien,diasVidaAlien.reduce((a, b) => a + b, 0)]
 }
 
 const aliensData1 = [1,1,1,1,2,1,1,4,1,4,3,1,1,1,1,1,1,1,1,4,1,3,1,1,1,5,1,3,1,4,1,2,1,1,5,1,1,1,1,1,1,1,1,1,1,3,4,1,5,1,1,1,1,1,1,1,1,1,3,1,4,1,1,1,1,3,5,1,1,2,1,1,1,1,4,4,1,1,1,4,1,1,4,2,4,4,5,1,1,1,1,2,3,1,1,4,1,5,1,1,1,3,1,1,1,1,5,5,1,2,2,2,2,1,1,2,1,1,1,1,1,3,1,1,1,2,3,1,5,1,1,1,2,2,1,1,1,1,1,3,2,1,1,1,4,3,1,1,4,1,5,4,1,4,1,1,1,1,1,1,1,1,1,1,2,2,4,5,1,1,1,1,5,4,1,3,1,1,1,1,4,3,3,3,1,2,3,1,1,1,1,1,1,1,1,2,1,1,1,5,1,3,1,4,3,1,3,1,5,1,1,1,1,3,1,5,1,2,4,1,1,4,1,4,4,2,1,2,1,3,3,1,4,4,1,1,3,4,1,1,1,2,5,2,5,1,1,1,4,1,1,1,1,1,1,3,1,5,1,2,1,1,1,1,1,4,4,1,1,1,5,1,1,5,1,2,1,5,1,1,1,1,1,1,1,1,1,1,1,1,3,2,4,1,1,2,1,1,3,2]
-const [aliens, totalAliens] = aliensWars(80,aliensData1)
+const [aliens, totalAliens] = aliensWars(18,aliensData1)
 console.log("Total de aliens 1 : " , totalAliens)
 
 const aliensData2 = [1,1,1,1,2,1,1,4,1,4,3,1,1,1,1,1,1,1,1,4,1,3,1,1,1,5,1,3,1,4,1,2,1,1,5,1,1,1,1,1,1,1,1,1,1,3,4,1,5,1,1,1,1,1,1,1,1,1,3,1,4,1,1,1,1,3,5,1,1,2,1,1,1,1,4,4,1,1,1,4,1,1,4,2,4,4,5,1,1,1,1,2,3,1,1,4,1,5,1,1,1,3,1,1,1,1,5,5,1,2,2,2,2,1,1,2,1,1,1,1,1,3,1,1,1,2,3,1,5,1,1,1,2,2,1,1,1,1,1,3,2,1,1,1,4,3,1,1,4,1,5,4,1,4,1,1,1,1,1,1,1,1,1,1,2,2,4,5,1,1,1,1,5,4,1,3,1,1,1,1,4,3,3,3,1,2,3,1,1,1,1,1,1,1,1,2,1,1,1,5,1,3,1,4,3,1,3,1,5,1,1,1,1,3,1,5,1,2,4,1,1,4,1,4,4,2,1,2,1,3,3,1,4,4,1,1,3,4,1,1,1,2,5,2,5,1,1,1,4,1,1,1,1,1,1,3,1,5,1,2,1,1,1,1,1,4,4,1,1,1,5,1,1,5,1,2,1,5,1,1,1,1,1,1,1,1,1,1,1,1,3,2,4,1,1,2,1,1,3,2]
